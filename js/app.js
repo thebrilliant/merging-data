4d2cd19655ca502fb517b57e656853f8a347fe73
viv <vivyanw@uw.edu>
2014-11-13 08:38:22 -0800

    fixed angular bracket bug and fixing ng-if statements

--- js/app.js
+++ js/app.js
@@ -6,12 +6,11 @@
 "use strict";
 
 //new angular module called Address Book
-angular.module('AddressBook', []) {
+angular.module('AddressBook', [])
 	.controller('AddressController', function($scope) {
 		//initializes a new variable with the list of employees in the address book
 		$scope.employees = pawneeEmployees;
 
 		//enables order by last name
 		$scope.order = 'lastname';
-	})
-};
+	});
